b321accbd0263cf0c21de1dc736bb5a43b6506fa
HoodedUnicorn <68126304+HoodedUnicorn@users.noreply.github.com>
2023-01-30 12:45:37 +0100

    Update CygwinUninstaller.bat

--- Cygwin Uninstaller/CygwinUninstaller.bat
+++ Cygwin Uninstaller/CygwinUninstaller.bat
@@ -1,6 +1,25 @@
 @echo off
 echo Running script as administrator
- /user:Administrator cmd /c "echo Deleting Cygwin shortcuts on the Desktop & del /f /q \"%USERPROFILE%\Desktop\Cygwin64.lnk\" & echo Deleting Cygwin shortcuts on the Start Menu & del /f /q \"%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\Cygwin*\" & echo Deleting registry node Software\\Cygnus Solutions & reg delete \"HKEY_LOCAL_MACHINE\SOFTWARE\Cygnus Solutions\" /f & reg delete \"HKEY_CURRENT_USER\SOFTWARE\Cygnus Solutions\" /f & rd /s /q C:\\cygwin & echo Deleting files created in the setup temporary directory & del /f /q %temp%\\cygwin-setup* & echo Done"
+
+echo Deleting Cygwin shortcuts on the Desktop
+del /f /q "Users\Public\Public Desktop\Cygwin64 Terminal.lnk"
 pause
 
+echo Deleting Cygwin shortcuts on the Start Menu
+del /f /q "Windows\Start Menu\Programs\Cygwin*"
+pause
 
+echo Deleting registry node Software\Cygnus Solutions
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Cygwin" /f
+pause
+
+echo Deleting Cygwin installation directory
+rd /s /q "C:\cygwin"
+pause
+
+echo Deleting files created in the setup temporary directory
+del /f /q "%temp%\cygwin-setup*"
+pause
+
+echo Done
+pause
